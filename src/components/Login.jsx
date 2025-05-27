2b0498dc721527ad93aa5a248a219917d24e5819
SSO <sso@d.co>
2025-05-27 18:51:32 +0200

    added input handle to password

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,57 @@
+import { useState } from 'react';
+
 export default function Login() {
-  return (
-    <form>
-      <h2>Login</h2>
+	const [enteredValues, setEnteredValues] = useState({
+		email: '',
+		password: '',
+	});
 
-      <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
-        </div>
+	function handleSubmit(e) {
+		e.preventDefault();
+		console.log('Submitted');
+		console.log('User email: ' + enteredValues.email);
+	}
 
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
-        </div>
-      </div>
+	function handleInputChange(identifier, value) {
+		setEnteredValues((prevValues) => ({
+			...prevValues,
+			[identifier]: value,
+		}));
+	}
+	return (
+		<form onSubmit={handleSubmit}>
+			<h2>Login</h2>
 
-      <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
-      </p>
-    </form>
-  );
+			<div className='control-row'>
+				<div className='control no-margin'>
+					<label htmlFor='email'>Email</label>
+					<input
+						id='email'
+						type='email'
+						name='email'
+						onChange={(e) => handleInputChange('email', e.target.value)}
+						value={enteredValues.email}
+					/>
+				</div>
+
+				<div className='control no-margin'>
+					<label htmlFor='password'>Password</label>
+					<input
+						id='password'
+						type='password'
+						name='password'
+						onChange={(e) => handleInputChange('password', e.target.value)}
+						value={enteredValues.password}
+					/>
+				</div>
+			</div>
+
+			<p className='form-actions'>
+				<button className='button button-flat'>Reset</button>
+				<button className='button' onClick={handleSubmit}>
+					Login
+				</button>
+			</p>
+		</form>
+	);
 }
